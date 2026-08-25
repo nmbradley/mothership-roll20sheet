d41de8f355e9d9656d0285fa8c4b52172dadedf8
Nic Bradley <nic.m.bradley@gmail.com>
2026-08-25 20:01:49 +0100

    [1e] 65 Add Annual Maintenance Check Button (#71)
    * Add Annual Maintenance Check and Bankruptcy logic
    
    * Refactor ship sheet into modular panels and reuse core components
    
    ---------
    
    Co-authored-by: Nic Bradley <nic@roll20.net>

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,25 @@
+import {
+  handleAnnualMaintenanceCheck,
+  handleBankruptcySave,
+  handleBattleCheck,
+  handleSystemsCheck,
+  handleThrustersCheck,
+} from "./rules/ships";
 import { incrementHighScore } from "./rules/stats";
 
-on(`clicked:increment_score`, incrementHighScore);
+on("clicked:increment_score", incrementHighScore);
+on("clicked:annual_maintenance", () => {
+  void handleAnnualMaintenanceCheck();
+});
+on("clicked:bankruptcy_save", () => {
+  void handleBankruptcySave();
+});
+on("clicked:systems_check", () => {
+  void handleSystemsCheck();
+});
+on("clicked:thrusters_check", () => {
+  void handleThrustersCheck();
+});
+on("clicked:battle_check", () => {
+  void handleBattleCheck();
+});
