1dd79bdcea7d350f31fb0abc8495363a3762fc7e
Nic Bradley <nic@roll20.net>
2026-08-25 12:24:58 +0100

    Chore: Create src/game, extract pug variables, move data, and add enums

---
+++ src/game/enums.ts
@@ -0,0 +1,26 @@
+export const Stats = {
+  Strength: "strength",
+  Speed: "speed",
+  Intellect: "intellect",
+  Combat: "combat",
+} as const;
+
+export const Saves = {
+  Sanity: "sanity",
+  Fear: "fear",
+  Body: "body",
+  Armor: "armor",
+} as const;
+
+export const Classes = {
+  Marine: "marine",
+  Android: "android",
+  Scientist: "scientist",
+  Teamster: "teamster",
+} as const;
+
+export const SkillLevels = {
+  Trained: "trained",
+  Expert: "expert",
+  Master: "master",
+} as const;
